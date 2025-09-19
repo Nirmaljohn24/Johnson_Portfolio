c92c2f1d0c8d9a85b48c1cf22ad774b48f2d6ae9
Nirmaljohn24 <johnsondravid143@gmail.com>
2025-09-19 23:00:26 +0530

    first commit

---
+++ src/Components/Navbar/Navabar.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Navabar = ({ theme }) => {
+  return (
+    <nav className="flex relative w-full h-20 md:px-[100px]  items-center overflow-hidden">
+      {/* Left background half */}
+      <div className="absolute top-0 left-0 w-[30%] h-full md:hidden bg-[#0bccef] z-0" />
+
+      {/* Right background half */}
+      <div className="absolute top-0 right-0 w-full h-full bg-[#0bccef] md:bg-transparent z-0" />
+
+      {/* Content */}
+      <div className="relative z-10 flex flex-shrink-0 items-center ">
+        <a href="/">
+          <h1
+            className={`${
+              theme === "dark"
+                ? "text-white md:text-yellow-300"
+                : " text-white md:text-[#0bccef]"
+            } text-xl lg:text-5xl font-bold font-dancing`}
+          >
+            <span
+              className={`mx-3 ${
+                theme === "dark" ? "md:text-white text-black  " : " md:text-black text-black "
+              }`}
+            >
+              Johnson Dravid
+            </span>
+            B
+          </h1>
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navabar;
---
+++ src/constants/index.js
@@ -0,0 +1,142 @@
+import project1 from "../assets/projects/bridalscreenshot.png";
+import project2 from "../assets/projects/wedding.png";
+import project3 from "../assets/projects/astro.png";
+import project4 from "../assets/projects/user1.png";
+import project5 from "../assets/projects/ecom.png";
+import project6 from "../assets/projects/pixalive1.png";
+import project7 from "../assets/projects/pixalive2.png";
+import project8 from "../assets/projects/pixalive3.png";
+import project9 from "../assets/projects/portfolio.png";
+import project10 from '../assets/projects/Dejavu.png'
+
+export const HERO1_CONTENT = `I am a Web Developer, UI Developer and MERN Stack Developer. I have rich experience in Frontend and Backend Development.`;
+
+export const HERO_CONTENT = `As a Full Stack Developer with nearly 2.10 years of experience, I specialize in building scalable web applications using the MERN stack (MongoDB, Express.js, React.js, Node.js). I have strong expertise in creating dynamic user interfaces, integrating them with robust back-end services through RESTful APIs and CRUD operations (GET, POST, UPDATE, DELETE), and managing databases for efficient data flow. I am adept at delivering responsive, secure, and high-performing applications with a focus on clean architecture, problem-solving, and maintainable code, while driving innovative solutions that enhance business growth and user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With nearly 2 years of professional experience, I have worked with a variety of technologies, including React, Node.js, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+const status = [
+  { label: "1+ Year's", description: "Experience" },
+  { label: "2 Projects +", description: "Completed" }
+];
+
+export const EXPERIENCES = [
+  {
+    year: "July2024 - Present",
+    role: "Software Developer",
+    company: "Pixalive Technology Service Private limited , Bangalore",
+    description: `Led a team in developing and maintaining web applications using JavaScript, React.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: ["React.js", "Tailwind", "Bootstrap", "ReduxToolKit", "Axios","Node.js","Express.js","MongoDb"]
+  },
+  {
+    year: "Dec 2022 - June 2024",
+    role: "Junior Software Developer",
+    company: "TripAlive.me Technology private limited",
+    description: `Designed and developed user interfaces for web applications using  React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+    technologies: ["React.js", "Tailwind", "Bootstrap", "ReduxToolKit", "Axios","Postman","Github","Netlify"]
+  }
+];
+
+export const EDUCATION = [
+  {
+    year: "2015 - 2019",
+    Graduate: "Under-Graduate - B.E",
+    University: "Jeppiaar SRR Engineering College "
+  },
+  {
+    year: "2013 - 2015",
+    Graduate: "Higher Secondary School ",
+    University: "CK School of Practical Knowledge "
+  },
+  {
+    year: "2012 - 2013",
+    Graduate: "SSLC ",
+    University: "St.Joseph's Hr.Sec School"
+  }
+];
+
+export const PROJECTS = [
+  {
+    title: "Bridal Studio UniSex Saloon & Spa",
+    image: project1,
+   
+    technologies: ["HTML", "CSS", "ReactJS", "Tailwindcss"],
+    link: "https://www.lafusionbridal.com/"
+  },
+  {
+    title: "Dejavu MakeUp",
+    image: project10,
+   
+    technologies: ["HTML", "CSS", "Reactjs", "TailwindCss"],
+    link: "https://dejavumakeup.com/"
+  },
+  {
+    title: "Wedding PhotoShop Studio",
+    image: project2,
+   
+    technologies: ["HTML", "CSS", "Reactjs", "TailwindCss"],
+    link: "https://wedding-photography-chennai.netlify.app/"
+  },
+  
+  {
+    title: "Big Bay Cinema Factory",
+    image: project9,
+   
+    technologies: ["ReactJS", "TailwindCss"],
+    link: "https://baycinemafactory.netlify.app/"
+  },
+  {
+    title: "Engg. Management System",
+    image: project4,
+   
+    technologies: ["ReactJS", "TailwindCss", "NodeJs"],
+    link: "https://management-system-wxxs.vercel.app/"
+  },
+  {
+    title: "Pixalive Tech Offical Website",
+    image: project6,
+    description:
+      "Pixalive is pioneering live streaming and digital innovation for the future of business.",
+    technologies: ["ReactJS", "TailwindCss", "Nodejs", "ExpressJs", "MongoDB"],
+    link: "https://app.pixalivetech.com/"
+  },
+  {
+    title: "Pixalive -CoWorks",
+    image: project7,
+    description:
+      "Whether you're a freelancer, a startup, or a remote team, we've built the perfect workspace around you. Modern desks, fast internet, and ready-to-use laptops.",
+    technologies: ["ReactJS", "TailwindCss", "Nodejs", "ExpressJs", "MongoDB"],
+    link: "https://www.co-works.pixalivetech.com/"
+  },
+  {
+    title: "Pixalive Gold Network",
+    image: project8,
+    description: "Secure Your Future with India’s First Social Gold Network.",
+    technologies: ["ReactJS", "TailwindCss", "Nodejs", "ExpressJs", "MongoDB"],
+    link: "https://gold.pixalivenetwork.com/"
+  },
+  {
+    title: "Drapify Store (Contact Us) Page",
+    image: project5,
+   
+    technologies: [
+      "ReactJS",
+      "TailwindCss"
+    ],
+    link: "https://drapify-store.netlify.app/"
+  },
+  {
+    title: "Astro Love",
+    image: project3,
+    
+    technologies: ["HTML", "CSS", "React", "TailwindCss"],
+    link: "https://chipper-panda-c0bfe6.netlify.app/"
+  },
+  
+];
+
+export const CONTACT = {
+  Location: "Bangalore, ElectroniCity Phase2",
+  phoneNo: "9943863916 ",
+  email: "sudhagarmsr712@gmail.com"
+};
